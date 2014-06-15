33c656943e22f61cca20bc20d115c0e07768ba8c
Taylor Kaplan <taylor.kaplan@allegiantair.com>
2014-06-15 16:23:57 -0700

    Basic test suite start

--- mockapp/app.js
+++ mockapp/app.js
@@ -8,4 +8,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+	res.sendfile('public/mock.html');
+});
+
 app.listen(process.env.PORT || 2999);
